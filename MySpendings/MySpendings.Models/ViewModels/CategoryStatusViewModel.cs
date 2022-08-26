0d597fd3cf69a5478db41c656609b21471ed94e5
Roman Cholkan <Rochohoho@gmail.com>
2022-08-26 23:39:42 +0300

    Create CategoryStatusViewModel.cs

---
+++ MySpendings/MySpendings.Models/ViewModels/CategoryStatusViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace MySpendings.Models.ViewModels
+{
+    public class CategoryStatusViewModel
+    {
+        [ValidateNever]
+        public Category Category { get; set; }
+
+        [ValidateNever]
+        public bool IsActive { get; set; }
+    }
+}
