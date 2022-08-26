3a44acd5c4772112628eeaa20cae3df690dac9a6
Roman Cholkan <Rochohoho@gmail.com>
2022-08-26 23:17:29 +0300

    Updated OutlayChartViewModel

--- MySpendings/MySpendings.Models/ViewModels/OutlayChartViewModel.cs
+++ MySpendings/MySpendings.Models/ViewModels/OutlayChartViewModel.cs
@@ -1,9 +1,29 @@
-﻿namespace MySpendings.Models.ViewModels
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MySpendings.Models.ViewModels
 {
     public class OutlayChartViewModel
     {
-        public Dictionary<string, float> CategoryOutlays { get; set; }
+        [ValidateNever]
+        public Dictionary<string, float> CurrentMonthCategoryOutlays { get; set; }
+
+        [ValidateNever]
         public string OutlaysData { get; set; }
-        public IEnumerable<Outlay> Outlays { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<Outlay> CurrentMonthOutlays { get; set; }
+
+        [ValidateNever]
+        public int SelectedMonth { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> MonthList { get; set; }
+
+        [ValidateNever]
+        public int SelectedYear { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> YearsList { get; set; }
     }
 }
