97e48a0310464ab46885b005d9110df89938b4e8
Roman Cholkan <Rochohoho@gmail.com>
2022-08-27 02:33:16 +0300

    Updated Account Controller. Added Settings page.

--- MySpendings/MySpendings.Web/Controllers/AccountController.cs
+++ MySpendings/MySpendings.Web/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MySpendings.DataAccess.Repository.IRepository;
 using MySpendings.Models;
@@ -11,10 +12,12 @@ namespace MySpendings.Web.Controllers
     public class AccountController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public AccountController(IUnitOfWork unitOfWork)
+        public AccountController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
 
         [HttpGet]
@@ -50,6 +53,9 @@ namespace MySpendings.Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterModel model)
         {
+            if (model.Income < 0)
+                ModelState.AddModelError("Income", "Income cannot be less than 0.");
+
             if (ModelState.IsValid)
             {
                 var user = await _unitOfWork.User.GetFirstOrDefaultAsync(u => u.Login == model.Login);
@@ -60,7 +66,9 @@ namespace MySpendings.Web.Controllers
                         Name = model.Name,
                         Login = model.Login,
                         Password = model.Password,
-                        Email = model.Email
+                        Email = model.Email,
+                        Income = model.Income,
+                        ImageUrl = "\\images\\profileImages\\blankImageProfile.png"
                     };
 
                     await _unitOfWork.User.AddAsync(newUser);
@@ -75,12 +83,65 @@ namespace MySpendings.Web.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Settings()
+        {
+            var currentUser = await _unitOfWork.User
+               .GetFirstOrDefaultAsync(u => u.Login == User.Identity.Name);
+
+            if (currentUser == null)
+                return RedirectToAction("Login", controllerName: "Account");
+
+            return View(currentUser);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Settings(User user, IFormFile? file)
+        {
+            if (user.Income < 0)
+                ModelState.AddModelError("Income", "Income cannot be less than 0.");
+
+            if (ModelState.IsValid)
+            {
+                var wwwRootPath = _hostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    var fileName = Guid.NewGuid().ToString();
+                    var uploadsPath = Path.Combine(wwwRootPath, @"images\profileImages");
+                    var extension = Path.GetExtension(file.FileName);
+
+                    if (!string.IsNullOrWhiteSpace(user.ImageUrl) && user.ImageUrl != "\\images\\profileImages\\blankImageProfile.png")
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, user.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                            System.IO.File.Delete(oldImagePath);
+                    }
+
+                    using (var fileStreams = new FileStream(Path.Combine(uploadsPath, fileName + extension), FileMode.Create))
+                        file.CopyTo(fileStreams);
+                    user.ImageUrl = @"\images\profileImages\" + fileName + extension;
+                }
+
+                TempData["Success"] = "Profile updated seccessfully";
+                _unitOfWork.User.Update(user);
+                await _unitOfWork.SaveAsync();
+                return RedirectToAction(nameof(Settings));
+            }
+            
+            return View(user);
+        }
+
+        [HttpGet]
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Login", "Account");
         }
 
+        #region Helpers Methods
         private async Task Authenticate(string userName)
         {
             
@@ -88,5 +149,6 @@ namespace MySpendings.Web.Controllers
             ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
         }
+        #endregion
     }
 }
